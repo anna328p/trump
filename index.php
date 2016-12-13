4bdd35e831421de5f69349774dcbe59d04ed5c29
Your Name <you@example.com>
2016-12-13 00:25:50 -0800

    Added jQuery, Materalize and default.css/js for head.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+
+<?php include 'resource/head.php' ?>
+
+<style>
+
+p{
+  font-size: 2em;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<p>This is a sample page for the TrumpTracker. If this is working, you should see this text in the pSans font.</p>
+
+</body>
+
+</html>
---
+++ resource/head.php
@@ -0,0 +1,5 @@
+<script src="resource/jquerymin311.js"></script>
+<script src="resource/materialize/js/materialize.min.js"></script>
+<script src="resource/default.js"></script>
+<link rel="stylesheet" type="text/css" href="resource/materialize/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="resource/default.css">
