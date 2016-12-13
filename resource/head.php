d53393c33acd64fa995c3a3e1aa241c86c4172e2
Your Name <joshfeinsilber@gmail.com>
2016-12-13 00:33:06 -0800

    Added comments in header to make it easer to read

--- resource/head.php
+++ resource/head.php
@@ -1,5 +1,14 @@
+<!--jQuery-->
 <script src="resource/jquerymin311.js"></script>
+
+<!--Materialize JS-->
 <script src="resource/materialize/js/materialize.min.js"></script>
+
+<!--Default.js-->
 <script src="resource/default.js"></script>
+
+<!--Materialize CSS-->
 <link rel="stylesheet" type="text/css" href="resource/materialize/css/materialize.min.css">
+
+<!--Default.js-->
 <link rel="stylesheet" type="text/css" href="resource/default.css">
