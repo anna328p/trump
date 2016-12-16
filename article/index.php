620d161f126f5fca2e1b4748b641525ad2d3c742
Student <student@Macs-Mac-Pro.local>
2016-12-16 10:50:16 -0800

    Got the head.php file working

--- article/index.php
+++ article/index.php
@@ -1,15 +1,27 @@
 <html>
 
 <head>
-
-<?php include "../resource/head.php" ?>
+<?php require '../resource/head.php' ?>
 
 </head>
-
 <body>
 
-<p>This is the article view.</p>
+<div class="header">
+  <h1>(Issue)</h1>
+  <br>
+  <br>
+  <h5>(Date)</h5>
+</div>
+<div class="container">
+  <div class="row">
+  <div class="col s6" id="summary">This div is 6-columns wide</div>
+  <div class="col s6" id="video">This div is 6-columns wide</div>
+  <div class="col s6" id="summary">This div is 6-columns wide</div>
+  <div class="col s6" id="video">This div is 6-columns wide</div>
 
+
+</div>
+</div>
 
 </body>
 
