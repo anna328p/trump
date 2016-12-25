143244adbfbad29a677dad8bcbece1664ac52336
Your Name <joshfeinsilber@gmail.com>
2016-12-24 21:30:20 -1000

    Got the basic login working. Needs more work

---
+++ home/comp/mainscore.php
@@ -0,0 +1,5 @@
+<div class="image" id ="mainCircle">
+
+<p>Yeah!</p>
+
+</div>
---
+++ home/index.php
@@ -0,0 +1,25 @@
+<?php require './../resource/head.php' ?>
+
+<head>
+
+<title>Welcome</title>
+
+<link rel="stylesheet" href="../home/css/style.css">
+
+</head>
+
+
+<body>
+
+<?php require '../resource/html/header.php' ?>
+
+<div class = "container">
+
+<?php require '../home/comp/mainscore.php' ?>
+
+
+
+</div>
+
+
+</body>
