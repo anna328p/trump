7a4e22229e916fbb66417c84f883af767cc97941
Your Name <joshfeinsilber@gmail.com>
2017-01-04 11:08:40 -0800

    Footer!

---
+++ resource/html/footer.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="/resource/css/footer.css">
+<footer class="footer-distributed">
+
+  <div class="footer-right">
+
+    <!--<a href="#"><i class="fa fa-facebook"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    <a href="#"><i class="fa fa-linkedin"></i></a>
+    <a href="#"><i class="fa fa-github"></i></a>-->
+
+  </div>
+
+  <div class="footer-left">
+
+    <p class="footer-links">
+      <a href="#">Home</a>
+      ·
+      <a href="#">Blog</a>
+      ·
+      <a href="#">About</a>
+      ·
+      <a href="#">Article</a>
+      ·
+      <a href="#">Contact</a>
+    </p>
+
+    <p>DonaldTracker &copy; 2017</p>
+  </div>
+
+</footer>
