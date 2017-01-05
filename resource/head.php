0608a22a696da7a8ecd5872b9839e15fd5114265
Your Name <joshfeinsilber@gmail.com>
2017-01-04 23:06:58 -0800

    Made the homepage iPhone web app friendly so it kinda acts like an app

--- resource/head.php
+++ resource/head.php
@@ -11,7 +11,7 @@ if($ignorelogin!='yes'){
 
 <head>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="apple-mobile-web-app-capable" content="yes">  
 
 <link rel="icon" href="/resource/images/logos/icons/blackicon.png" type="image/gif" sizes="16x16">
 
