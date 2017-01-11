42952e185f8582ffafc1a360925808e42a276315
Your Name <joshfeinsilber@gmail.com>
2017-01-10 21:18:37 -0800

    Typo on about page. Dang typo.

--- pages/about/comp/bottomMessage.php
+++ pages/about/comp/bottomMessage.php
@@ -1,2 +1,2 @@
 <hr>
-<div class = "bottomMessage monospace center">This page was thrown together at last second. Like...last, last second. That's why our answers our in audio form! A more detailed about page is coming soon...in text. We promise!</div>
+<div class = "bottomMessage monospace center">This page was thrown together at last second. Like...last, last second. That's why our answers are in audio form! A more detailed about page is coming soon...in text. We promise!</div>
