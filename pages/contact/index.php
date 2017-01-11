3128ce473653efdd5e664e91efb0d1025f4ae4e6
Your Name <joshfeinsilber@gmail.com>
2017-01-11 09:23:11 -0800

    Contact page

---
+++ pages/contact/index.php
@@ -0,0 +1,66 @@
+<?php
+ $ignorelogin = 'yes';
+ $material = 'block';
+ require '../../resource/head.php' ?>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Contact Us</title>
+
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+    <?php require '../../resource/html/header.php' ?>
+  <div class = "card"><aside class="profile-card">
+  <header>
+    <!-- here’s the avatar -->
+    <a href="/">
+      <img src="../../resource/images/logos/icons/blackicon.png" class="hoverZoomLink">
+    </a>
+
+    <!-- the username -->
+    <h1>
+            DonaldTracker
+          </h1>
+
+    <!-- and role or location -->
+    <h2>
+            Seattle, WA
+          </h2>
+
+  </header>
+
+  <!-- bit of a bio; who are you? -->
+  <div class="profile-bio">
+
+    <p>
+      Created by some students in Washington State, DonaldTracker provides the most accurate and honest view of America's perception of Trump. Made with love in Seattle.
+    </p>
+
+  </div>
+
+  <!-- some social links to show off -->
+  <ul class="profile-social-links">
+    <li>
+      <a target="_blank" href="https://www.facebook.com/DonaldTracker">
+        <i class="fa fa-facebook"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://twitter.com/DonaldTracker">
+        <i class="fa fa-twitter"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="mailto:donaldtrackerofficial@gmail.com?Subject=Hey%20there!">
+        <i class="fa fa-envelope"></i>
+      </a>
+    </li>
+  </ul>
+</aside>
+</body>
