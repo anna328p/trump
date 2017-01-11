65f8c5d09b66bb1096468662f885a9865b1ac916
Your Name <joshfeinsilber@gmail.com>
2017-01-11 11:01:44 -0800

    New article view

--- article/comp/summary.php
+++ article/comp/summary.php
@@ -1,3 +1,4 @@
-<div class='summary'>
-  summary
+<h3>Summary</h3>
+<div class="summaryContent">
+  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
 </div>
