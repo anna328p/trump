ecd90231e02faec21267eadaea8537d0081f8f54
Your Name <joshfeinsilber@gmail.com>
2017-01-17 15:14:06 -0800

    Recent article shows the recent article

--- home/comp/recentarticle.php
+++ home/comp/recentarticle.php
@@ -2,7 +2,7 @@
   <table>
     <tr>
       <td><a href="../article?w=<?php echo $latestArticleWeek?>"><div class = "recentEvent monospace"><?php echo $latestArticleName ?></div></a></td>
-      <td><div class = "recentEventDate monospace"><?php echo $latestArticleDate?></div></td>
+      <td><div class = "recentEventDate monospace"><?php echo strtok($latestArticleDate, ',')?></div></td>
     </tr>
     <tr>
       <td><div class = "monospace voteAvailable">Voting for this topic is still available.</div></td>
